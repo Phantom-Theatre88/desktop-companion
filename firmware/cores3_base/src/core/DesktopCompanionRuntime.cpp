0bf391d6ba40120ac4af59fe8d709144d3175fc0
Hiro <fq1500@yahoo.co.jp>
2026-09-17 08:07:50 +0900

    Wire semantic neurons into Ghost and reflex layers

---
+++ firmware/cores3_base/src/core/DesktopCompanionRuntime.cpp
@@ -0,0 +1,73 @@
+#include "DesktopCompanionRuntime.h"
+
+namespace deskbot {
+namespace core {
+
+bool DesktopCompanionRuntime::begin(uint32_t now_ms) {
+  ghost_.begin(now_ms);
+
+  bool ok = true;
+
+  const nerve::NeuronType ghost_inputs[] = {
+      nerve::NeuronType::PROXIMITY_NEAR,
+      nerve::NeuronType::PROXIMITY_APPROACHING,
+      nerve::NeuronType::PROXIMITY_LEAVING,
+      nerve::NeuronType::PERSON_PRESENT,
+      nerve::NeuronType::PERSON_ABSENT,
+      nerve::NeuronType::TOUCH,
+      nerve::NeuronType::STROKE_DETECTED,
+      nerve::NeuronType::PICKED_UP,
+      nerve::NeuronType::SHAKE,
+      nerve::NeuronType::FACE_DETECTED,
+      nerve::NeuronType::FACE_LOST,
+      nerve::NeuronType::LOUD_SOUND,
+      nerve::NeuronType::VOICE_ACTIVITY,
+  };
+
+  for (const auto type : ghost_inputs) {
+    ok = connectGhost(type) && ok;
+  }
+
+  const nerve::NeuronType reflex_inputs[] = {
+      nerve::NeuronType::PROXIMITY_NEAR,
+      nerve::NeuronType::PROXIMITY_APPROACHING,
+      nerve::NeuronType::PERSON_PRESENT,
+      nerve::NeuronType::FACE_DETECTED,
+      nerve::NeuronType::LOUD_SOUND,
+      nerve::NeuronType::PICKED_UP,
+      nerve::NeuronType::SHAKE,
+  };
+
+  for (const auto type : reflex_inputs) {
+    ok = connectReflex(type) && ok;
+  }
+
+  return ok;
+}
+
+void DesktopCompanionRuntime::tick(uint32_t now_ms) {
+  ghost_.tick(now_ms);
+}
+
+void DesktopCompanionRuntime::emit(const nerve::SemanticNeuron& neuron) {
+  synapse_.emit(neuron);
+}
+
+bool DesktopCompanionRuntime::connectGhost(nerve::NeuronType type) {
+  return synapse_.connect(
+      type,
+      nerve::SynapseTarget::GHOST,
+      ghost::GhostCore::synapseHandler,
+      &ghost_);
+}
+
+bool DesktopCompanionRuntime::connectReflex(nerve::NeuronType type) {
+  return synapse_.connect(
+      type,
+      nerve::SynapseTarget::REFLEX,
+      reflex::ReflexLayer::synapseHandler,
+      &reflex_);
+}
+
+}  // namespace core
+}  // namespace deskbot
