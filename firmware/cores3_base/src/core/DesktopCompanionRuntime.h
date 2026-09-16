92b113cb670a5cfe403dbad748a9411f010d86c1
Hiro <fq1500@yahoo.co.jp>
2026-09-17 08:07:42 +0900

    Add Desktop Companion neural runtime

---
+++ firmware/cores3_base/src/core/DesktopCompanionRuntime.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <Arduino.h>
+#include "../ghost/GhostCore.h"
+#include "../nerve/SynapseRouter.h"
+#include "../reflex/ReflexLayer.h"
+
+namespace deskbot {
+namespace core {
+
+class DesktopCompanionRuntime {
+ public:
+  bool begin(uint32_t now_ms);
+  void tick(uint32_t now_ms);
+  void emit(const nerve::SemanticNeuron& neuron);
+
+  nerve::SynapseRouter& synapse() { return synapse_; }
+  ghost::GhostCore& ghost() { return ghost_; }
+  reflex::ReflexLayer& reflex() { return reflex_; }
+
+ private:
+  bool connectGhost(nerve::NeuronType type);
+  bool connectReflex(nerve::NeuronType type);
+
+  nerve::SynapseRouter synapse_{};
+  ghost::GhostCore ghost_{};
+  reflex::ReflexLayer reflex_{};
+};
+
+}  // namespace core
+}  // namespace deskbot
