7498636dd2b39dca92049ed39dd8c5bb95a0e1e7
Hiro <fq1500@yahoo.co.jp>
2026-09-17 17:50:04 +0900

    Refactor GhostCore around production Ghost components

--- firmware/cores3_base/src/ghost/GhostCore.h
+++ firmware/cores3_base/src/ghost/GhostCore.h
@@ -1,26 +1,15 @@
 #pragma once
 
 #include <Arduino.h>
+#include "BehaviorEngine.h"
+#include "HeartEngine.h"
+#include "MemoryEngine.h"
+#include "RelationshipEngine.h"
+#include "TimeEngine.h"
 #include "../nerve/NerveTypes.h"
 
 namespace deskbot {
 namespace ghost {
-
-struct HeartState {
-  float mood = 0.5f;
-  float affection = 0.5f;
-  float curiosity = 0.5f;
-  float boredom = 0.0f;
-  float sleepiness = 0.0f;
-  float attention = 0.0f;
-};
-
-struct ShortMemory {
-  nerve::NeuronType last_type = nerve::NeuronType::NONE;
-  nerve::NeuronSource last_source = nerve::NeuronSource::UNKNOWN;
-  uint32_t last_event_ms = 0;
-  uint32_t event_count = 0;
-};
 
 class GhostCore {
  public:
@@ -28,15 +17,24 @@ class GhostCore {
   void onNeuron(const nerve::SemanticNeuron& neuron);
   void tick(uint32_t now_ms);
 
-  const HeartState& heart() const { return heart_; }
-  const ShortMemory& memory() const { return memory_; }
+  const HeartState& heart() const { return heart_.state(); }
+  HeartContext heartContext(uint32_t now_ms) const { return heart_.snapshot(now_ms); }
+  const ShortMemory& memory() const { return memory_.shortMemory(); }
+
+  HeartEngine& heartEngine() { return heart_; }
+  MemoryEngine& memoryEngine() { return memory_; }
+  TimeEngine& timeEngine() { return time_; }
+  RelationshipEngine& relationshipEngine() { return relationship_; }
+  BehaviorEngine& behaviorEngine() { return behavior_; }
 
   static void synapseHandler(const nerve::SemanticNeuron& neuron, void* context);
 
  private:
-  HeartState heart_{};
-  ShortMemory memory_{};
-  uint32_t last_tick_ms_ = 0;
+  HeartEngine heart_{};
+  MemoryEngine memory_{};
+  TimeEngine time_{};
+  RelationshipEngine relationship_{};
+  BehaviorEngine behavior_{};
 };
 
 }  // namespace ghost
