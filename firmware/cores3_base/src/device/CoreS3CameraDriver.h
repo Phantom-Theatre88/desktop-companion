ca205c2f87d6b7f75e74abaf12a13c81c46a2bfd
Hiro <fq1500@yahoo.co.jp>
2026-09-18 00:16:57 +0900

    Add reusable camera frame capture boundary

--- firmware/cores3_base/src/device/CoreS3CameraDriver.h
+++ firmware/cores3_base/src/device/CoreS3CameraDriver.h
@@ -5,22 +5,34 @@
 namespace deskbot {
 namespace device {
 
-struct CameraProbeResult {
+struct CameraFrameView {
+  const uint8_t* data = nullptr;
+  size_t bytes = 0;
+  uint16_t width = 0;
+  uint16_t height = 0;
+  uint32_t timestamp_ms = 0;
+};
+
+class CameraFrameConsumer {
+ public:
+  virtual ~CameraFrameConsumer() = default;
+  virtual void onCameraFrame(const CameraFrameView& frame) = 0;
+};
+
+struct CameraCaptureResult {
   bool initialized = false;
   bool captured = false;
   bool internal_i2c_restored = false;
   uint16_t width = 0;
   uint16_t height = 0;
   size_t bytes = 0;
-  uint8_t average_luma = 0;
+  uint32_t timestamp_ms = 0;
 };
 
 class CoreS3CameraDriver {
  public:
-  CameraProbeResult probeOnce();
-
- private:
-  static uint8_t estimateLumaRgb565(const uint8_t* data, size_t bytes);
+  CameraCaptureResult captureOnce(uint32_t now_ms,
+                                  CameraFrameConsumer* consumer = nullptr);
 };
 
 }  // namespace device
