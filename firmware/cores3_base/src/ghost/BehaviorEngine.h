fe7a4c0d41224a74c2f55ab49993ab06d47dd694
Hiro <fq1500@yahoo.co.jp>
2026-09-17 22:29:30 +0900

    Add standalone DeskRobo micro-behavior frame

--- firmware/cores3_base/src/ghost/BehaviorEngine.h
+++ firmware/cores3_base/src/ghost/BehaviorEngine.h
@@ -7,17 +7,34 @@
 namespace deskbot {
 namespace ghost {
 
+struct MicroBehaviorFrame {
+  float eye_openness = 0.90f;
+  float gaze_x = 0.0f;
+  float gaze_y = 0.0f;
+  float left_eye_bias = 0.0f;
+  float right_eye_bias = 0.0f;
+  bool blink_active = false;
+  uint32_t generated_ms = 0;
+};
+
 class BehaviorEngine {
  public:
-  void begin(uint32_t now_ms) { (void)now_ms; }
+  void begin(uint32_t now_ms);
 
   void onNeuron(const nerve::SemanticNeuron& neuron,
-                const HeartContext& event_context) {
-    (void)neuron;
-    (void)event_context;
-  }
+                const HeartContext& event_context);
 
-  void tick(uint32_t now_ms) { (void)now_ms; }
+  void tick(uint32_t now_ms, const HeartContext& heart_context);
+
+  const MicroBehaviorFrame& microBehavior() const { return micro_behavior_; }
+
+ private:
+  static float clamp01(float value);
+  static float clampSigned(float value);
+
+  uint32_t started_ms_ = 0;
+  uint32_t last_event_ms_ = 0;
+  MicroBehaviorFrame micro_behavior_{};
 };
 
 }  // namespace ghost
