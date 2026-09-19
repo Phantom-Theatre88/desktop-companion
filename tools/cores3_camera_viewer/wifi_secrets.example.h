856e85087c71a8a0587bbf6e2798d1dd20f1ab41
Hiro <fq1500@yahoo.co.jp>
2026-09-19 22:03:22 +0900

    Add local Wi-Fi secrets template for camera viewer

---
+++ tools/cores3_camera_viewer/wifi_secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Copy this file to wifi_secrets.h on your Mac.
+// wifi_secrets.h is ignored by Git and must never be committed.
+
+constexpr char WIFI_SSID[] = "YOUR_WIFI_SSID";
+constexpr char WIFI_PASSWORD[] = "YOUR_WIFI_PASSWORD";
